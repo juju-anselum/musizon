c45a295933b9fdde1750807b1a9a7fe1bd69f6bf
juju-anselum <anselumjuju@gmail.com>
2024-07-22 18:37:19 +0530

    Scrolling problem solved.

--- src/components/cards/AlbumCard1.jsx
+++ src/components/cards/AlbumCard1.jsx
@@ -9,7 +9,7 @@ const AlbumCard1 = ({ data }) => {
 
   return (
     <div className='min-w-48 p-2 flex flex-col items-start gap-2 rounded-md transition-all duration-100 hover:bg-primaryColorAccent cursor-pointer' onClick={handleNavigate}>
-      <img src={data.posterURL} alt={data.albumName} className='w-full rounded-md object-cover' />
+      <img src={data.posterURL} alt={data.albumName} className='w-full aspect-square rounded-md object-cover' />
       <div className="w-full h-max flex flex-col items-start gap-1">
         <p className='w-full text-md truncate text-left text-secondaryColorAccent font-normal lg:font-medium'>{data.albumName}</p>
         <p className='w-full text-xs truncate text-left text-secondaryColorAccent font-normal lg:font-medium'>{data.artistName}</p>
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
 import { auth } from '../api/auth/firebase';
 import { useNavigate, Outlet } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
@@ -8,6 +9,10 @@ import { fetchPreferencesAsync } from "../redux/slices/firestoreSlice";
 const MainLayout = () => {
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
+	const location = useLocation();
+
+	const mobileContainerRef = useRef(null);
+	const desktopContainerRef = useRef(null);
 
 	const preferences = useSelector(state => state.fireStore?.preferences);
 	const status = useSelector(state => state.fireStore?.status);
@@ -18,9 +23,14 @@ const MainLayout = () => {
 				navigate('/login');
 			} else {
 				dispatch(fetchPreferencesAsync());
+				if (location.pathname.includes('/song') || location.pathname.includes('/artist') || location.pathname.includes('/album')) {
+					console.log('Scrolling: ' + location.pathname);
+					mobileContainerRef.current?.scrollTo(0, 0);
+					desktopContainerRef.current?.scrollTo(0, 0);
+				}
 			}
 		});
-	}, [dispatch, navigate]);
+	}, [dispatch, navigate, location.pathname]);
 
 	useEffect(() => {
 		if (status === 'Fetched' && preferences?.name === '') {
@@ -38,7 +48,7 @@ const MainLayout = () => {
 					</div>
 					<Navbar />
 				</div>
-				<div className='w-full h-full pb-40 p-4 overflow-y-scroll no-scrollbar'>
+				<div ref={mobileContainerRef} className='w-full h-full pb-40 p-4 overflow-y-scroll no-scrollbar'>
 					<Outlet />
 				</div>
 			</div>
@@ -48,7 +58,7 @@ const MainLayout = () => {
 					<div className='w-max h-full'>
 						<Navbar />
 					</div>
-					<div className='flex-1 h-full overflow-y-scroll no-scrollbar'>
+					<div ref={desktopContainerRef} className='flex-1 h-full overflow-y-scroll no-scrollbar'>
 						<Outlet />
 					</div>
 				</div>
